71fbb2c137a40842b1d686ba63f35a82ec2030bc
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-02 16:17:01 +0000

    Add character controller

---
+++ Assets/scripts/CharacterControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class CharacterControl : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed;
+	[SerializeField]
+	private float jumpHeight;
+
+	private Rigidbody rb;
+	private Animator anim;
+
+	private void Start () {
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+	}
+
+	private void Update () {
+		Vector3 moveChar = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0, CrossPlatformInputManager.GetAxis("Vertical"));
+		transform.position += moveChar * moveSpeed * Time.deltaTime;
+	}
+}
