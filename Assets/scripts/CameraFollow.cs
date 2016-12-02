e45835204eadd704004b34180574e1e95f6f98d7
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-02 12:24:49 +0000

    Create camera follow script

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+	[SerializeField]
+	private float distance;
+	[SerializeField]
+	private float targetHeight;
+
+	private float x = 0;
+	private float y = 0;
+	
+	private void LateUpdate () {
+		if (!target) {
+			return;
+		}
+
+		y = target.eulerAngles.y;
+		Quaternion rot = Quaternion.Euler(x, y, 0);
+		transform.rotation = rot;
+
+		Vector3 pos = target.position - (rot * Vector3.forward * distance + new Vector3(0, -targetHeight, 0));
+		transform.position = pos;
+	}
+}
